45de7f66b80333208670c12e823728592bdb5d05
apareslar <zerod_mlg@hotmail.com>
2020-11-27 10:26:20 +0100

    New version of 'graphrevealer.rb'

--- bin/graphrevealer.rb
+++ bin/graphrevealer.rb
@@ -2,6 +2,87 @@
 
 require 'optparse'
 require 'ruby-graphviz'
+require 'zlib'
+
+############################################################################################
+## METHODS
+############################################################################################
+def header_parser(str)
+	arr = str.split("\t")
+	arr.map! do |attr|
+	    if attr == 'subject'
+	        attr = 'id'
+	    elsif attr.include?('subject_')
+	        attr = attr.split('subject_')[1]
+	    elsif attr == 'object'
+	        attr = ""
+	    elsif attr.include?('object_')
+	        attr = attr.split('object_')[1]
+	    else
+	    	attr = attr    
+	    end    
+	end
+	arr.reject!(&:empty?)    
+end
+
+def all_values_equal?(hash)
+    values_count_grouped = hash.map {|k,v| [k, v.count]}.to_h.group_by{|k,v| v}
+    size = values_count_grouped.values.size
+    if size != 1
+    	puts "WARNING!! NOT ALL HEADERS HAVE EQUAL LENGTH:"
+    	size.times {|i| values_count_grouped.values[i].map{|file, args| puts "#{file} \t #{args} attributes"}}
+    else
+    	puts "All headers have equal length"
+    end		
+end
+
+def get_headerNcompare(file_list)
+	headers = {}
+	file_list.each do |file|
+		Zlib::GzipReader.open(file) { |gz|
+			headers[file] = header_parser(gz.readlines[0].chomp)
+		}	
+	end
+	all_values_equal?(headers)
+	return headers		
+end
+
+def makegraph(arr_nodes, output_path, file_attributes)
+	common_attr = file_attributes.values[0]
+	diff_attr = file_attributes["/mnt/home/users/bio_267_uma/apareslar/projects/Db_documents/test/monarch_graph/tsv/all_associations/disease_phenotype.all.tsv.gz"]
+
+	## Creating graph and setting global attributes ##
+	g = GraphViz.new( :G, :type => :digraph )
+	g.node[:shape] = "record"
+
+	## Assigning the edges ##
+	included_nodes = []
+	arr_nodes.each_with_index do |nodeAnodeB, i|
+		nodeA, nodeB = nodeAnodeB
+		relation = g.add_nodes("relation_#{i}")
+		relation[:color] = "green"
+		relation[:label] = "{ #{nodeA}_#{nodeB} |{#{common_attr[5]}| #{common_attr[6]}| #{common_attr[7]}| #{common_attr[8]}| #{common_attr[9]}| #{common_attr[10]}| #{common_attr[11]}}}"
+		if !included_nodes.include?(nodeA)
+			included_nodes << nodeA
+			node_A = g.add_nodes(nodeA)
+			if nodeA != 'diesease'
+				node_A[:label] = "{ #{nodeA} |{#{common_attr[0]}| #{common_attr[1]}| #{common_attr[2]}| #{common_attr[3]}| #{common_attr[4]}}}"
+			else
+				node_A[:label] = "{ #{nodeA} |{#{diff_attr[0]}| #{diff_attr[1]}| #{diff_attr[2]}| #{diff_attr[3]}| #{diff_attr[4]} #{diff_attr[12]}| #{diff_attr[13]}| #{diff_attr[14]}| #{diff_attr[15]}}}"
+			end	
+		end
+		if !included_nodes.include?(nodeB)
+			included_nodes << nodeB
+			node_B= g.add_nodes(nodeB)
+			node_B[:label] = "{ #{nodeB} |{#{common_attr[0]}| #{common_attr[1]}| #{common_attr[2]}| #{common_attr[3]}| #{common_attr[4]}}}"
+		end
+		g.add_edges(nodeA, relation)
+		g.add_edges(relation, nodeB)
+	end
+	## Saving the final graph ##
+	g.output(:png => "#{output_path}.png")
+	puts "Graph succesfully created: #{output_path}.png"	
+end
 
 ############################################################################################
 ## OPTPARSE
@@ -34,21 +115,9 @@ files.each do |file_path|
 	all_nodes << array_nodes_file
 end
 
-g = GraphViz.new( :G, :type => :digraph )
+puts "Getting the headers and looking for differences..."
+headers = get_headerNcompare(files)
+put headers
 
-## Assigning the edges ##
-included_nodes = []
-all_nodes.each do |nodeA, nodeB|
-	if !included_nodes.include?(nodeA)
-		included_nodes << nodeA
-		g.add_nodes(nodeA)
-	end
-	if !included_nodes.include?(nodeB)
-		included_nodes << nodeB
-		g.add_nodes(nodeB)
-	end
-	g.add_edges(nodeA, nodeB)
-end
 
-## Saving the final graph ##
-g.output(:png => "#{options[:output]}.png")	
+makegraph(all_nodes, options[:output], headers)
--- test/monarch_graph/monarch_graph_dig.sh
+++ test/monarch_graph/monarch_graph_dig.sh
@@ -3,5 +3,5 @@
 source ~soft_bio_267/initializes/init_ruby
 
 #wget --recursive --no-parent --no-verbose --no-host-directories --level=1 --cut-dirs=1 --reject 'md5sums' --accept *.all.tsv.gz https://archive.monarchinitiative.org/202010/tsv/all_associations/
-mkdir monarch_output_data
+#mkdir monarch_output_data
 ../../bin/graphrevealer.rb -o 'monarch_output_data/monarch_graph' -i /mnt/home/users/bio_267_uma/apareslar/projects/Db_documents/test/monarch_graph/tsv/all_associations/
